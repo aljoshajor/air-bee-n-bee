f9414ec45386e6631851eab940aedc7e86d4abeb
marian heinzmann <faraskan@gmx.net>
2020-08-18 16:16:01 +0200

    Adding files, required for the booking creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 	devise_for :users
-	root to: 'pages#home'
-	get 'bees/new', to: 'bees#new'
-	get 'bees/:id', to: 'bees#show', as: "bee"
-	get 'bees', to: 'bees#index'
-	post 'bees', to: 'bees#create'
+  root to: 'pages#home'
+  resources :bees do
+    resources :bookings, only: [:new, :create]
+  end
+
+
+	# get 'bees/new', to: 'bees#new'
+	# get 'bees/:id', to: 'bees#show', as: "bee"
+	# get 'bees', to: 'bees#index'
+  # post 'bees', to: 'bees#create'
+  # get 'bookings/new', to: 'bookings#new'
+  # post 'bookings', to: 'bookings#create'
 	
 
 
