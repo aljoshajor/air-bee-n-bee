074daeb9d9c31e0fba65ffc884e28dab88aeaed0
joseph welling <joseph.welling@hotmail.com>
2020-08-18 15:20:10 +0200

    add form done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-  get 'bees/new', to: 'bees#new'
-  get 'bees/:id', to: 'bees#show'
-  get 'bees', to: 'bees#index'
-  
+	devise_for :users
+	root to: 'pages#home'
+	get 'bees/new', to: 'bees#new'
+	get 'bees/:id', to: 'bees#show', as: "bee"
+	get 'bees', to: 'bees#index'
+	post 'bees', to: 'bees#create'
+	
+
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
