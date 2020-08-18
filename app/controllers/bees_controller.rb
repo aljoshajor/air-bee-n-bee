4c89be5d43ed14618d37f780174c6141c7ade9cb
marian heinzmann <faraskan@gmx.net>
2020-08-18 14:35:35 +0200

    Created action, route & view for NEW

--- app/controllers/bees_controller.rb
+++ app/controllers/bees_controller.rb
@@ -5,4 +5,25 @@ class BeesController < ApplicationController
 	def show 
 		@bee = Bee.find(params[:id])
 	end
+
+	def new
+		@bee = Bee.new
+	end
+
+	def create
+		@bee = Bee.new(bee_params)
+		@bee.user_id = current_user.id
+			if	@bee.save
+				redirect_to bee_path(@bee)
+			else
+				render :new
+			end
+	end
+
+
+	private
+
+	def bee_params 
+		params.require(:bee).permit(:name, :description, :user_id)
+	end
 end
