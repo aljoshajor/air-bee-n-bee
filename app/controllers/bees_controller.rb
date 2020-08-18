5646734853996715f093db67ae35fa86d2d3323c
marian heinzmann <faraskan@gmx.net>
2020-08-18 14:41:43 +0200

    made minor changes (order of actions)

--- app/controllers/bees_controller.rb
+++ app/controllers/bees_controller.rb
@@ -1,29 +1,29 @@
 class BeesController < ApplicationController
 	def index		
-	end
+		end
 
-	def show 
-		@bee = Bee.find(params[:id])
-	end
+		def new
+			@bee = Bee.new
+		end
 
-	def new
-		@bee = Bee.new
-	end
+		def create
+			@bee = Bee.new(bee_params)
+			@bee.user_id = current_user.id
+				if	@bee.save
+					redirect_to bee_path(@bee)
+				else
+					render :new
+				end
+		end
 
-	def create
-		@bee = Bee.new(bee_params)
-		@bee.user_id = current_user.id
-			if	@bee.save
-				redirect_to bee_path(@bee)
-			else
-				render :new
-			end
-	end
+		def show 
+			@bee = Bee.find(params[:id])
+		end
 
 
-	private
+		private
 
-	def bee_params 
-		params.require(:bee).permit(:name, :description, :user_id)
-	end
+		def bee_params 
+			params.require(:bee).permit(:name, :description, :user_id)
+		end
 end
