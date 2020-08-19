4a894b76b2e0348e6176fc2598a77d7a7c5addcc
joseph welling <joseph.welling@hotmail.com>
2020-08-20 00:18:19 +0200

    a lot

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,24 @@
+class LikesController < ApplicationController
+	before_action :find_bee
+
+	def create
+		if already_liked?
+			flash[:notice] = "You can't like a bee more than once"
+		else
+			@bee.likes.create(user_id: current_user.id)
+			redirect_to bee_path(@bee)
+		end
+	end  
+
+	private  
+
+	def find_bee
+		@bee = Bee.find(params[:bee_id])
+	end
+
+	def already_liked?
+		Like.where(user_id: current_user.id, bee_id:  
+			params[:bee_id]).exists?
+	end
+
+end
